4fefaa532c2ec300a581f46b9afc4edb233acabc
ZippyDaFish <andrew.riker21@gmail.com>
2023-04-01 14:08:10 -0400

    Fix background overflow issue

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,10 @@ import '../styles/Title.css';
 const Title = (props) => {
     return(
         <div className='title'>
-            <h1>My Hero Memory!</h1>
+            <div>
+                <h1>My Hero Memory!</h1>
+                <h3>Try to click all the cards, but don't click one more than once!</h3>
+            </div>
             <div>
                 <h3>Score: {props.score}</h3>
                 <h3>High Score: {props.highScore}</h3>
